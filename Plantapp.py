81208f34ec9b7d3cdf893f6680e47ddaff1aaff3
Trishalsai <101546656+Trishalsai@users.noreply.github.com>
2023-05-15 09:30:49 +0200

    Update Plantapp.py

--- Plantapp.py
+++ Plantapp.py
@@ -9,8 +9,7 @@ from tensorflow.keras.models import load_model
 
 app = FastAPI()
 origins = [
-    "http://130.89.87.169:19000",  
-    "http://localhost:3000",   
+    "http://130.89.87.169:19000",   
 ]
 # Set up CORS middleware
 app.add_middleware(
